4bdd04d744ae89f55a3b3d42f11308ed23f9cffb
tomposbea <tompos.beata@yahoo.com>
2022-06-24 07:00:42 +0000

    write, build, script

--- build2/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/etombea/BencmarkingTool
-# Build directory: /home/etombea/BencmarkingTool/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("eric-test/eric-test")
-subdirs("test")
