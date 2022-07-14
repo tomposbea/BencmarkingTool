489f3f2ce77542ac0ef64ff22d7790bf770f151b
tomposbea <tompos.beata@yahoo.com>
2022-07-14 12:31:35 +0000

    dut37 results

---
+++ scripts/matrix_row.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd ../build
+
+if [[ $# -eq 0 ]]
+then
+        echo Please input new run counter value, tip: 0 or previous value + 1
+        exit 1
+fi
+
+counter=$1
+echo Run counter: $counter
+
+for row in {2..5}; do
+            echo "MATRIX SIZE: $row x 3"
+            counter=$((counter+1))
+            ./Run -runtime 500 -row $row -column 3 -counter $counter -csv MatrixRow_500 -xml MatrixRow_500
+done;
+
+echo Last run counter: $counter
